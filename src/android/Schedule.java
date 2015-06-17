f578727b6e41f7b4ecb4db61af6cf5e121d90fc5
anupam-chatterjee <anupam@vrisini.com>
2015-06-17 20:31:58 +0530

    Fixed package typo
    Fixed package typo

--- src/android/Schedule.java
+++ src/android/Schedule.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.schedule;
+package vrisini.cordova.plugin.schedule;
 
 import java.util.ArrayList;
 import java.util.Map;
